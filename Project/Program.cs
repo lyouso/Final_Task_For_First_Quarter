8617dfef0770e0fea5e2340dcc17fbbb12c73f0b
Liubov K <lyouso@yandex.ru>
2022-05-08 15:00:58 +0300

    Created a cycle for result array

--- Project/Program.cs
+++ Project/Program.cs
@@ -8,3 +8,13 @@
 
 string[] result = new string[array.Length];
 int rezultSize = 0;
+
+foreach (string value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[rezultSize] = value;
+        Console.Write(result[rezultSize] + "\t");
+        rezultSize++;
+    }
+}
